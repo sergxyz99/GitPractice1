bb92e4d816566dfd2c7808d09ff92696aae713af
zeniuk <T26q58Pj7b>
2019-12-03 15:54:26 +0300

    model 1 commit

--- src/com/zs/gitpractice/p1/Person.java
+++ src/com/zs/gitpractice/p1/Person.java
@@ -1,6 +1,31 @@
 package com.zs.gitpractice.p1;
 
 public class Person {
+    Long id;
     String firstName;
     String surName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
 }
